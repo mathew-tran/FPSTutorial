70200b42a331ef09d4e5e6eb1e268d655ebc1edd
mathew <mathew.tran.dev@gmail.com>
2025-08-22 13:12:36 +0800

    following tutorial v1

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput mPlayerInput;
+    private PlayerInput.OnFootActions mOnFootActions;
+
+    private PlayerController mPlayerController;
+
+    private void Awake()
+    {
+        mPlayerInput = new PlayerInput();
+        mOnFootActions = mPlayerInput.OnFoot;
+        mPlayerController = GetComponent<PlayerController>();
+        mOnFootActions.Jump.performed += ctx => mPlayerController.Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        mPlayerController.ProcessLook(mOnFootActions.Look.ReadValue<Vector2>());
+        mPlayerController.ProcessMove(mOnFootActions.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        mOnFootActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        mOnFootActions.Disable();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController mController;
+    private Vector3 mVelocity;
+    public float mSpeed;
+
+    private bool bIsGrounded = true;
+    public float mGravity = -9.8f;
+    public float mJumpStrength = 10.0f;
+
+    public Camera mCamera;
+    private float mXRotation = 0f;
+    public float mXSensitivity = 30.0f;
+    public float mYSensitivity = 30.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bIsGrounded = mController.isGrounded;
+    }
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        mXRotation -= (mouseY * Time.deltaTime) * mYSensitivity;
+        mXRotation = Mathf.Clamp(mXRotation, -60.0f, 60.0f);
+
+        mCamera.transform.localRotation = Quaternion.Euler(mXRotation, 0, 0);
+
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * mXSensitivity);
+
+    }
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        mController.Move(transform.TransformDirection(moveDirection) * mSpeed * Time.deltaTime);
+        mVelocity.y += mGravity * Time.deltaTime;
+        if (bIsGrounded && mVelocity.y < 0)
+        {
+            mVelocity.y = -2f;
+        }
+        mController.Move(mVelocity * Time.deltaTime);
+        Debug.Log(mVelocity);
+
+    }
+
+    public void Jump()
+    {
+        if (bIsGrounded)
+        {
+            mVelocity.y = Mathf.Sqrt(mJumpStrength * -1 * mGravity);
+        }
+    }
+}
